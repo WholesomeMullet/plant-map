72235b9ce6bb8eda558c739d6774928ed6837f13
YuseonChoi <chldbtjs74@gmail.com>
2021-09-09 12:38:05 +0900

    #9 Scene to Map / Camera Update

--- Assets/Scripts/TransferMap.cs
+++ Assets/Scripts/TransferMap.cs
@@ -8,11 +8,16 @@ public class TransferMap : MonoBehaviour
 
     public string transferMapName;  // 이동할 맵의 이름
 
+    public Transform target;
+    public BoxCollider2D targetBound;
+
     private MovingObject thePlayer;
+    private CameraManager theCamera;
 
     // Start is called before the first frame update
     void Start()
     {
+        theCamera = FindObjectOfType<CameraManager>();
         thePlayer = FindObjectOfType<MovingObject>();
     }
 
@@ -21,7 +26,10 @@ public class TransferMap : MonoBehaviour
         if (collision.gameObject.name == "Player")
         {
             thePlayer.currentMapName = transferMapName;
-            SceneManager.LoadScene(transferMapName);
+            theCamera.SetBound(targetBound);
+            theCamera.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, theCamera.transform.position.z);
+            thePlayer.transform.position = target.transform.position;
+            
         }
     }
 }
---
+++ Assets/Scripts/TransferScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransferScene : MonoBehaviour
+{
+    public string transferMapName;  // 이동할 맵의 이름
+
+    private MovingObject thePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<MovingObject>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)  // Box Collider에 닿는 순간 실행되는 함수
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            thePlayer.currentMapName = transferMapName;
+            SceneManager.LoadScene(transferMapName);
+        }
+    }
+}
