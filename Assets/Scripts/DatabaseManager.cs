7a00b0616a429b720b30bf7e7ebd28ddeb7da11e
YuseonChoi <chldbtjs74@gmail.com>
2021-09-13 01:02:26 +0900

    #11 Create Item list

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -10,22 +10,13 @@ public class Item
     public string itemDescription;  // 아이템 설명
     public int itemCount;           // 소지 개수
     public Sprite itemIcon;         // 아이템의 아이콘
-    public ItemType itemType;
-    public enum ItemType
-    {
-        Use,
-        Equip,
-        Quest,
-        ETC
-    }
 
-    public Item(int _itemID, string _itemName, string _itemDes, ItemType _itemType, int _itemCount = 1)
+    public Item(int _itemID, string _itemName, string _itemDes, int _itemCount = 1)
     {
 
         itemID = _itemID;
         itemName = _itemName;
         itemDescription = _itemDes;
-        itemType = _itemType;
         itemCount = _itemCount;
         itemIcon = Resources.Load("ItemIcon/" + _itemID.ToString(), typeof(Sprite)) as Sprite;
     }
